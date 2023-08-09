61b19572c1496505f424c9e052434541f7e90c3d
Phalandia <139664162+phalandia@users.noreply.github.com>
2023-08-08 17:15:41 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log("if you're reading this, you're gay.");
+window.alert("welcome to the unofficial oily black men website");
+
+let men;
+men = "we have 1234 men";
+let men2;
+men2 = "1234";
+let trv = true;
+let oil;
+oil = "are you oily? "
+
+console.log(men);
+console.log("hello,", oil)
+console.log("Are you oily? Answer:", trv)
+
+document.getElementById("p2").innerHTML = "Mommy, " + oil + "Answer: " + trv
+document.getElementById("p3").innerHTML = "Are you oily test answer: " + trv
+document.getElementById("p4").innerHTML = "How many (oily) men do you have here? " + men2
